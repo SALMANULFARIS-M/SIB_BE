98ba7852dba73c56d28d0555b2c8e6e7883f3f1e
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-02-22 16:55:45 +0530

    cors allowed domains

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ import adminRoute from './routes/admin_routes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS options
+const corsOptions = {
+  origin: ['http://localhost:4200', 'https://studyinbengaluru.com'], // Replace with your allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+};
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
